bfd2fa0dfeea09712225012307819232991c66d4
Ibrahim Khalilullah <01754488189ib@gmail.com>
2026-05-19 21:50:31 +0600

    create a DrawerSidebarPage

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const page = () => {
+const DrawerSidebarPage = () => {
     return (
         <div>
-            <h2>Dashboard</h2>
+            <div className="drawer">
+                <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default page;
+export default DrawerSidebarPage;
